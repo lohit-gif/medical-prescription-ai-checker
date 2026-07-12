e37e3bb10d74c43f8c80a5188c5fd6a86aa974f1
D Lohit <lohit.22.2007@gmail.com>
2026-07-12 15:03:19 +0530

    fix: allow all hosts to fix 400 Bad Request on Render

--- medical_project/settings.py
+++ medical_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key-change-i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 # Trust Render's HTTPS domain for CSRF
 CSRF_TRUSTED_ORIGINS = [
